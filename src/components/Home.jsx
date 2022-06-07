8399a6dc00b27809288e3d1ef1333b36e165da88
Megan Ratto <megratt@gmail.com>
2022-06-07 11:11:05 -0400

    Update home button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className='homeButton'>
-      <Link to='/today' className='homeLink'>Enter the stars!</Link>
+      <Link to='/today' className='homeLink'>Explore</Link>
       <p>View Nasa's photo of the day, or search past daily photos.</p>
     </div>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,18 +4,18 @@ const NavBar = () => {
   const location = useLocation();
 
   return (
-    <nav className='navbar'>
-      <div>NASA API</div>
-        {location.pathname === '/'  ?
-          <ul></ul>
-        :
+    <>
+      {location.pathname !== '/'  &&
+        <nav className='navbar'>
+          <div>NASA API</div>
           <ul className='navlist'>
             <li><NavLink to='/'>Home</NavLink></li>
             <li><NavLink to='/search'>Search Past Photos</NavLink></li>
             <li><NavLink to='/today'>Today's Photo</NavLink></li>
           </ul>
-        }  
-    </nav>
+        </nav>
+      }  
+    </>
   )
 }
 
